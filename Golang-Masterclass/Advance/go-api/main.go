0568184ef1644239f6e23793d938041ddddf100b
newlinedeveloper <newlinedeveloper@gmail.com>
2023-05-27 16:31:16 +0530

    Resolve cors issue

--- Golang-Masterclass/Advance/go-api/main.go
+++ Golang-Masterclass/Advance/go-api/main.go
@@ -10,16 +10,26 @@ import (
 	routes "github.com/newlinedeveloper/go-api/Routes"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func main() {
 	router := mux.NewRouter()
 
+	// Enable CORS using rs/cors middleware
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"}, 
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Authorization", "Content-Type"},
+	})
+
 	// MongoDB Connection
-	configs.ConnectDB()
+	// configs.ConnectDB()
 
 	routes.MemberRoutes(router)
 
-	fmt.Print("Server is running on port 8000 !!!!")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	handler := c.Handler(router)
+
+	fmt.Print("Server is running on port 8000 !!!")
+	log.Fatal(http.ListenAndServe(":8000", handler))
 }
